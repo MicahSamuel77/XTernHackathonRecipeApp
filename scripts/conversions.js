bb94fcdd00c3c24fa5bda896addb4990c4f8e155
Neaves, Maeby <maeby@outlook.com>
2025-06-27 13:29:05 -0400

    conversion things and visual fixes

--- scripts/conversions.js
+++ scripts/conversions.js
@@ -1,38 +1,128 @@
-function convertFromTbsp() {
-    const tbspInput = document.getElementById("tablespoons");
-    const tspInput = document.getElementById("teaspoons");
-    const mlInput = document.getElementById("milliliters");
-
-    if (isNaN(tbspInput.value)) {
-        tbspInput.value = "";
-    } else {
-        tspInput.value = tbspInput.value*3;
-        mlInput.value = Math.round(10 * tbspInput.value * 14.787) / 10
+function convertToCups(amount, measurement) {
+    switch(measurement) {
+        case "ml":
+            return amount / 236.59;
+        case "tsp":
+            return amount / 48;
+        case "tbsp":
+            return amount / 16;
+        case "floz":
+            return amount / 8;
+        case "pints":
+            return amount * 2;
+        case "quarts":
+            return amount * 4;
+        case "gallons":
+            return amount * 16;
     }
 }
 
-function convertFromTsp() {
-    const tbspInput = document.getElementById("tablespoons");
-    const tspInput = document.getElementById("teaspoons");
-    const mlInput = document.getElementById("milliliters");
-
-    if (isNaN(tspInput.value)) {
-        tspInput.value = "";
-    } else {
-        tbspInput.value = Math.round(10 * tspInput.value / 3) / 10;
-        mlInput.value = Math.round(10 * tspInput.value * 4.929) / 10
+function convertFromCups(amount, measurement) {
+    switch(measurement) {
+        case "ml":
+            return amount * 236.59;
+        case "tsp":
+            return amount * 48;
+        case "tbsp":
+            return amount * 16;
+        case "floz":
+            return amount * 8;
+        case "pints":
+            return amount / 2;
+        case "quarts":
+            return amount / 4;
+        case "gallons":
+            return amount / 16;
     }
 }
 
-function convertFromMl() {
-    const tbspInput = document.getElementById("tablespoons");
-    const tspInput = document.getElementById("teaspoons");
-    const mlInput = document.getElementById("milliliters");
+function updateInputs(amount, measurement) {
+    const amountInCups = (measurement == "cups" ? amount : convertToCups(amount, measurement));
 
-    if (isNaN(mlInput.value)) {
-        mlInput.value = "";
-    } else {
-        tspInput.value = Math.round(10 * mlInput.value / .203) / 10;
-        tbspInput.value = Math.round(10 * mlInput.value / .068) / 10
+    if (measurement != "floz") {
+        const flOzInput = document.getElementById("fluidoz");
+        flOzInput.value = Math.round(100 * convertFromCups(amountInCups, "floz")) / 100;
     }
+    if (measurement != "cups") {
+        const cupsInput = document.getElementById("cups");
+        cupsInput.value = Math.round(100 * amountInCups) / 100;
+    }
+    if (measurement != "cups") {
+        const pintsInput = document.getElementById("pints");
+        pintsInput.value = Math.round(100 * convertFromCups(amountInCups, "pints")) / 100;
+    }
+    if (measurement != "quarts") {
+        const quartsInput = document.getElementById("quarts");
+        quartsInput.value = Math.round(100 * convertFromCups(amountInCups, "quarts")) / 100;
+    }
+    if (measurement != "gallons") {
+        const gallonsInput = document.getElementById("gallons");
+        gallonsInput.value = Math.round(100 * convertFromCups(amountInCups, "gallons")) / 100;
+    }
+    if (measurement != "tbsp") {
+        const tbspInput = document.getElementById("tablespoons");
+        tbspInput.value = Math.round(100 * convertFromCups(amountInCups, "tbsp")) / 100;
+    }
+    if (measurement != "tsp") {
+        const tspInput = document.getElementById("teaspoons");
+        tspInput.value = Math.round(100 * convertFromCups(amountInCups, "tsp")) / 100;
+    }
+    if (measurement != "ml") {
+        const mlInput = document.getElementById("milliliters");
+        mlInput.value = Math.round(100 * convertFromCups(amountInCups, "ml")) / 100;
+    }
+}
+
+function updateFromTbsp() {
+    const tbspInput = document.getElementById("tablespoons");
+    updateInputs(tbspInput.value, "tbsp");
+}
+
+function updateFromFlOz() {
+    const flOzInput = document.getElementById("fluidoz");
+    updateInputs(flOzInput.value, "floz");
+}
+
+function updateFromTsp() {
+    const tspInput = document.getElementById("teaspoons");
+    updateInputs(tspInput.value, "tsp");
+}
+
+function updateFromMl() {
+    const mlInput = document.getElementById("milliliters");
+    updateInputs(mlInput.value, "ml");
+}
+
+function updateFromCups() {
+    const cupsInput = document.getElementById("cups");
+    updateInputs(cupsInput.value, "cups");
+}
+
+function updateFromPints() {
+    const pintsInput = document.getElementById("pints");
+    updateInputs(pintsInput.value, "pints");
+}
+
+function updateFromQuarts() {
+    const quartsInput = document.getElementById("quarts");
+    updateInputs(quartsInput.value, "quarts");
+}
+
+function updateFromGallons() {
+    const gallonsInput = document.getElementById("gallons");
+    updateInputs(gallonsInput.value, "gallons");
+}
+
+function updateFromF() {
+    const fInput = document.getElementById("fahrenheit");
+    const cInput = document.getElementById("celsius");
+
+    cInput.value = Math.round(100 * (fInput.value - 32) / 1.8) / 100
+}
+
+function updateFromC() {
+    const fInput = document.getElementById("fahrenheit");
+    const cInput = document.getElementById("celsius");
+
+    fInput.value = Math.round(100 * (cInput.value * 1.8) + 32) / 100
 }
